7276fce1b02519dd844bbd7c8a09aa639d7ada23
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-04-22 11:32:41 +0100

    Initial commit
    Project configured with webpack, html template and stylesheet.
    Header / main / footer structure added to html.

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+import topBannerImg from "./assets/images/top-banner.jpg";
+
+import PhoneImg from "./assets/images/phone.svg";
+
+/* 
+
+Accessibility checklist - record what I do, to summarise elsewhere:
+
+  1. Semantic HTML:
+    header, main, fotter, sections, etc.
+
+  2. Use of alts for all images
+
+
+*/
+
+/* 
+
+Stages of approach:
+
+1. Overall framework of 3 sections
+  - put some media query framework structure in
+  - use accessible objects
+
+2. Top section:
+  - diagonal background
+  - read into how to have text wrap around image
+  - layered absolute image (I think)
+  - go full design with media queries
+  - different images for different widths/resolutions
+
+3. Middle section:
+  - Aim to do this fully through grid.
+  - Even number of projects should mean a flexible grid is plenty without media queries
+  - Possibly just margins in media queries, not the grid itself
+  - Should be able to create a project that will not differ by screen size
+    - may need to adjust text size or screenshot quality?
+
+4. Bottom section:
+  - Media queries for each design.
+  - Careful on text wrapping
+  - Otherwise broadly similar with two elements - text and image
+  - Use different images in different screen sizes
+
+*/
